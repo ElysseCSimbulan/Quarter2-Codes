9383cbf718d17f172f8a52ca29af1aa43330c130
ElysseCSimbulan <julianneelysse.simbulan@smc.pshs.edu.ph>
2025-10-11 00:25:10 +0800

    Add files via upload
    This code contains my answer to CS2Q2 Study Guide 1 Programming Task 2: Delivery Fee Calculator. The user will input the distance in kilometers and rate per kilometer in Philippine peso. The code will display the total fee. A user-defined function was utilized.

---
+++ SG1_PT2.py
@@ -0,0 +1,9 @@
+def calc_fee(distance, rate):
+    total_fee = distance*rate
+    return total_fee
+    
+distance = float(input("Enter distance in kilometers: "))
+rate = float(input("Enter rate per kilometer (₱): "))
+
+fee = calc_fee(distance, rate)
+print(f"Total Delivery Fee: ₱{fee:.2f}")
